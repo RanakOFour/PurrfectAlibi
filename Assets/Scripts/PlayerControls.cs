c790a66d0a7cc8aad9634244cb5a8bd1f093e392
RanakOFour <owrost@gmail.com>
2024-10-26 13:54:03 +0100

    5th
    FLUFFY

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -14,21 +14,24 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if(rb.velocity.magnitude < 5)
         {
-            rb.AddForce(new Vector3(0, 0, 1), ForceMode.VelocityChange);
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            rb.AddForce(new Vector3(0, 0, -1), ForceMode.VelocityChange);
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.AddForce(new Vector3(-1, 0, 0), ForceMode.VelocityChange);
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.AddForce(new Vector3(1, 0,0), ForceMode.VelocityChange);
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.AddForce(0.25f * Vector3.forward, ForceMode.VelocityChange);
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                rb.AddForce(-0.25f * Vector3.forward, ForceMode.VelocityChange);
+            }
+            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.AddForce(-0.25f * Vector3.right, ForceMode.VelocityChange);
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.AddForce(0.25f * Vector3.right, ForceMode.VelocityChange);
+            }
         }
     }
 }
